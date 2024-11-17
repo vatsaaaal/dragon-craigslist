a2b06d188b3c4ffcf4a626891376b97ed9e05dbf
Hieu Dang <hieudang.jeff2108@gmail.com>
2024-11-16 22:14:16 -0500

    Resolved App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -9,18 +8,7 @@ import Registration from './pages/Registration';
 import SignIn from './pages/SignIn';
 import LandingPage from './pages/Landing';
 import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
-=======
-import React from "react";
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import ChatPage from "./pages/Chat/ChatPage";
-import ChatRoom from "./pages/Chat/ChatRoom";
-import Registration from "./pages/Registration";
-import SignIn from "./pages/SignIn";
-import LandingPage from "./pages/Landing";
 import MarketplacePage from "./pages/Marketplace";
->>>>>>> main
 
 function App() {
   return (
