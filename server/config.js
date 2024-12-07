ff630b24de6eca4398c239baf8291edbbde9f9aa
longingia <vatsalomegalol@gmail.com>
2024-12-07 16:31:02 -0500

    deploy server and db on render

--- server/config.js
+++ server/config.js
@@ -1,8 +1,24 @@
 import fs from "fs";
 import path from "path";
 
-const envConfig = JSON.parse(
-  fs.readFileSync(path.resolve("env.json"), "utf-8")
-);
+// Check if running in production
+let envConfig = {};
+if (process.env.NODE_ENV === "production") {
+  // Use Render environment variables
+  envConfig = {
+    PG_USER: process.env.PG_USER,
+    PG_HOST: process.env.PG_HOST,
+    PG_DATABASE: process.env.PG_DATABASE,
+    PG_PASSWORD: process.env.PG_PASSWORD,
+    PG_PORT: process.env.PG_PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    FRONTEND_URL: process.env.FRONTEND_URL, // Add FRONTEND_URL when known
+  };
+} else {
+  // Load env.json for local development
+  envConfig = JSON.parse(
+    fs.readFileSync(path.resolve("env.json"), "utf-8")
+  );
+}
 
 export const config = envConfig;
