76e4f159b042e25a2b70a6e720cc7c0e1441d2df
longingia <vj92@drexel.edu>
2024-12-07 19:10:53 -0500

    deploy

--- src/pages/Chat/ChatList.jsx
+++ src/pages/Chat/ChatList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { fetchUserId } from "../../hooks/useFetchUserId";
-import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import "./ChatList.css";
 
